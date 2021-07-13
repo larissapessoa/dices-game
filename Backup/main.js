055195446f461ece5384c753a39a362ce6ed72ce
Gabriel Isaac Gonçalves Haydar <gabriel.isaac22@gmail.com>
2021-07-13 17:06:10 -0400

    Integrando as Páginas Parte 1 - Gabriel

--- Backup/main.js
+++ Backup/main.js
@@ -53,30 +53,32 @@ function rollDice(i) {
 
     if (i == 0) {
         document.getElementsByClassName("fas fa-dice-one")[0].className = "fas fa-dice-" + rollString;
-        setTimeout(function () { document.getElementsByClassName("fas fa-dice-" + rollString)[0].className = "fas fa-dice-one"; }, 2000);
+        setTimeout(function () { document.getElementsByClassName("fas fa-dice-" + rollString)[0].className = "fas fa-dice-one"; }, 5000);
     }
     else if (i == 1) {
         document.getElementsByClassName("fas fa-dice-two")[0].className = "fas fa-dice-" + rollString;
         if (randNum < 2) {
-            setTimeout(function () { document.getElementsByClassName("fas fa-dice-" + rollString)[1].className = "fas fa-dice-two"; }, 2000 + i * 50);
+            setTimeout(function () { document.getElementsByClassName("fas fa-dice-" + rollString)[1].className = "fas fa-dice-two"; }, 5000);
         }
         else {
-            setTimeout(function () { document.getElementsByClassName("fas fa-dice-" + rollString)[0].className = "fas fa-dice-two"; }, 2000 + i * 50);
+            setTimeout(function () { document.getElementsByClassName("fas fa-dice-" + rollString)[0].className = "fas fa-dice-two"; }, 5000);
 
         }
     }
     else {
         document.getElementsByClassName("fas fa-dice-three")[0].className = "fas fa-dice-" + rollString;
         if (randNum < 3) {
-            setTimeout(function () { document.getElementsByClassName("fas fa-dice-" + rollString)[1].className = "fas fa-dice-three"; }, 2000 + i*50);
+            setTimeout(function () { document.getElementsByClassName("fas fa-dice-" + rollString)[1].className = "fas fa-dice-three"; }, 5000);
         }
         else {
-            setTimeout(function () { document.getElementsByClassName("fas fa-dice-" + rollString)[0].className = "fas fa-dice-three"; }, 2000 + i * 50);
+            setTimeout(function () { document.getElementsByClassName("fas fa-dice-" + rollString)[0].className = "fas fa-dice-three"; }, 5000);
+
         }
     }
     return randNum;
 }
-
+// set initial side
+rollDice();
 //----------------------------------------------------------------------------------------------------------------------//
 
 //Jogadores
@@ -96,16 +98,22 @@ function adicionarJogador(NovoJogador){ //max 4 jogadores
     console.log(listaJogadores);
 }
 
-function removerJogador(jogador) {
-    if (listaJogadores.length > 1) {
-        let index = listaJogadores.indexOf(jogador.nome == jogador);
-        listaJogadores.splice(index, 1);
+function vencedor(){ //retorna o objeto do jogador com maior pontuacao
+    let maiorPontuacao = 0;
+    let indexMaior = 0;
+    for(i=0; i<4; i++){
+        if(listaJogadores[i].pontuacao > maiorPontuacao){
+            maiorPontuacao = listaJogadores[i].pontuacao;
+            indexMaior = i;
+        }
     }
+    return listaJogadores[indexMaior];
+    //tratar empate*
+
 }
 
-
 //Lançamentos
-let rodadaAtual = 1; //parametro
+let qtdLancamentos = 0; //parametro
 let indexJogadorAtual = 0; //jogador jogando no momento]
 
 function atualizarPlacar() {
@@ -119,12 +127,8 @@ function atualizarPlacar() {
         document.getElementById("player-three-score").innerHTML = listaJogadores[indexJogadorAtual].pontuacao.toString() + " pontos";
     }
     else {
-        document.getElementById("player-four-score").innerHTML = listaJogadores[indexJogadorAtual].pontuacao.toString() + " pontos";
+        document.getElementById("player-one-score").innerHTML = listaJogadores[indexJogadorAtual].pontuacao.toString() + " pontos";
     }
-}
-
-function atualizarRodada() {
-    document.getElementById("rodada-atual").innerHTML = "Rodada " + rodadaAtual.toString() + " de " + numRodadas.toString();
 }
 
 function proximoJogador(){
@@ -132,73 +136,13 @@ function proximoJogador(){
         indexJogadorAtual = indexJogadorAtual + 1;
     }else{
         indexJogadorAtual = 0;
-        rodadaAtual = rodadaAtual + 1;
-        if (rodadaAtual <= numRodadas) {
-            atualizarRodada();
-        }
     }
     console.log("Jogador atual: " + listaJogadores[indexJogadorAtual].nome);
 }
 
-function vencedor() { //retorna o objeto do jogador com maior pontuacao
-    let maiorPontuacao = 0;
-    let indexMaior = 0;
-    for (i = 0; i < listaJogadores.length; i++) {
-        if (listaJogadores[i].pontuacao > maiorPontuacao) {
-            maiorPontuacao = listaJogadores[i].pontuacao;
-            indexMaior = i;
-        }
-    }
-    let vencedor = listaJogadores[indexMaior];
-    removerJogador(vencedor.nome);
-    return vencedor;
-    //tratar empate*
-
-}
-
-let primeiroColocado;
-let segundoColocado;
-let terceiroColocado;
-let quartoColocado;
-
-function fimDeJogo() {
-    primeiroColocado = vencedor();
-    localStorage.setItem("primeiroColocadoNome", primeiroColocado.nome);
-    localStorage.setItem("primeiroColocadoPontuacao", primeiroColocado.nome);
-    if (numJogadores == 4) {
-        segundoColocado = vencedor();
-        terceiroColocado = vencedor();
-        quartoColocado = vencedor();
-
-        localStorage.setItem("segundoColocadoNome", segundoColocado.nome);
-        localStorage.setItem("terceiroColocadoNome", terceiroColocado.nome);
-        localStorage.setItem("quartoColocadoNome", quartoColocado.nome);
-        localStorage.setItem("segundoColocadoPontuacao", segundoColocado.nome);
-        localStorage.setItem("terceiroColocadoPontuacao", terceiroColocado.nome);
-        localStorage.setItem("quartoColocadoPontuacao", quartoColocado.nome);
-    }
-    else if (numJogadores == 3) {
-        segundoColocado = vencedor();
-        terceiroColocado = vencedor();
-        localStorage.setItem("segundoColocadoNome", segundoColocado.nome);
-        localStorage.setItem("terceiroColocadoNome", terceiroColocado.nome);
-        localStorage.setItem("segundoColocadoPontuacao", segundoColocado.nome);
-        localStorage.setItem("terceiroColocadoPontuacao", terceiroColocado.nome);
-    }
-    else if (numJogadores == 2){
-        segundoColocado = vencedor();
-        localStorage.setItem("segundoColocadoNome", segundoColocado.nome);
-        localStorage.setItem("segundoColocadoPontuacao", segundoColocado.nome);
-    }
-
-
-    window.location.href = "finalresults.html";
-
-}
-
 function lancarDado(){
-    if (listaJogadores.length > 0) {
-        if (rodadaAtual <= numRodadas) {
+    if(listaJogadores.length > 0){
+        if(qtdLancamentos <= numRodadas*numDados*listaJogadores.length){
             let i = 0;
             let ultimoLancamento = 0;
             while (i < numDados) {
@@ -206,11 +150,12 @@ function lancarDado(){
                 listaJogadores[indexJogadorAtual].pontuacao = listaJogadores[indexJogadorAtual].pontuacao + ultimoLancamento;
                 i = i + 1;
             }  
+            qtdLancamentos = qtdLancamentos + numDados;
             atualizarPlacar();
             proximoJogador(); 
-        }
-        else {
-            fimDeJogo();
+        }else{
+            let vencedor = Vencedor();
+            console.log("Vencedor é: " + vencedor);
         } 
     }else{
         //faz nada
@@ -233,7 +178,6 @@ let nomeJogador1 = "";
 let nomeJogador2 = "";
 let nomeJogador3 = "";
 let nomeJogador4 = "";
-
 
 document.getElementById("one-dice").onclick = function (e) {
     numDados = 1;
@@ -312,9 +256,9 @@ function onloadTournamentData() {
     nomeJogador2 = localStorage.getItem("nomeJogador2");
     nomeJogador3 = localStorage.getItem("nomeJogador3");
     nomeJogador4 = localStorage.getItem("nomeJogador4");
-    numDados = parseInt(localStorage.getItem("numDados"));
-    numJogadores = parseInt(localStorage.getItem("numJogadores"));
-    numRodadas = parseInt(localStorage.getItem("numRodadas"));
+    numDados = localStorage.getItem("numDados");
+    numJogadores = localStorage.getItem("numJogadores");
+    numRodadas = localStorage.getItem("numRodadas");
    
     document.getElementById("rodada-atual").innerHTML = "Rodada 1 de " + numRodadas.toString();
     
@@ -345,7 +289,7 @@ function onloadTournamentData() {
         adicionarJogador(nomeJogador2);
         adicionarJogador(nomeJogador3);
     }
-    else {
+    else if (numJogadores == 2) {
         document.getElementById("player-four").innerHTML = "";
         document.getElementById("player-four-score").innerHTML = "";
         document.getElementById("player-three").innerHTML = "";
@@ -353,6 +297,15 @@ function onloadTournamentData() {
         document.getElementById("player-two").innerHTML = nomeJogador2;
         adicionarJogador(nomeJogador1);
         adicionarJogador(nomeJogador2);
+    }
+    else {
+        document.getElementById("player-four").innerHTML = "";
+        document.getElementById("player-four-score").innerHTML = "";
+        document.getElementById("player-three").innerHTML = "";
+        document.getElementById("player-three-score").innerHTML = "";
+        document.getElementById("player-two").innerHTML = "";
+        document.getElementById("player-two-score").innerHTML = "";
+        adicionarJogador(nomeJogador1);
     }
 
     console.log(numJogadores);
@@ -363,49 +316,4 @@ function onloadTournamentData() {
     console.log(numDados);
     console.log(numRodadas);
     console.log(listaJogadores);
-}
-
-function onloadFinalResultsData() {
-    numJogadores = parseInt(localStorage.getItem("numJogadores"));
-    primeiroColocado = [localStorage.getItem("primeiroColocadoNome"), localStorage.getItem("primeiroColocadoPontuacao")];
-
-    document.getElementById("first-place").innerHTML = primeiroColocado[0];
-    document.getElementById("first-place-score").innerHTML = primeiroColocado[1] + " pontos";
-
-
-    if (numJogadores == 4) {
-        quartoColocado = [localStorage.getItem("quartoColocadoNome"), localStorage.getItem("quartoColocadoPontuacao")];
-        terceiroColocado = [localStorage.getItem("terceiroColocadoNome"), localStorage.getItem("terceiroColocadoPontuacao")];
-        segundoColocado = [localStorage.getItem("segundoColocadoNome"), localStorage.getItem("segundoColocadoPontuacao")];
-
-        document.getElementById("fourth-place").innerHTML = quartoColocado[0];
-        document.getElementById("third-place").innerHTML = terceiroColocado[0];
-        document.getElementById("second-place").innerHTML = segundoColocado[0];
-        document.getElementById("fourth-place-score").innerHTML = quartoColocado[1] + " pontos";
-        document.getElementById("third-place-score").innerHTML = terceiroColocado[1] + " pontos";
-        document.getElementById("second-place-score").innerHTML = segundoColocado[1] + " pontos";
-    }
-    else if (numJogadores == 3) {
-        terceiroColocado = [localStorage.getItem("terceiroColocadoNome"), localStorage.getItem("terceiroColocadoPontuacao")];
-        segundoColocado = [localStorage.getItem("segundoColocadoNome"), localStorage.getItem("segundoColocadoPontuacao")];
-
-        document.getElementById("fourth-place").innerHTML = "";
-        document.getElementById("fourth-place-score").innerHTML = "";
-
-        document.getElementById("third-place").innerHTML = terceiroColocado[0];
-        document.getElementById("second-place").innerHTML = segundoColocado[0];
-        document.getElementById("third-place-score").innerHTML = terceiroColocado[1] + " pontos";
-        document.getElementById("second-place-score").innerHTML = segundoColocado[1] + " pontos";
-    }
-    else {
-        segundoColocado = [localStorage.getItem("segundoColocadoNome"), localStorage.getItem("segundoColocadoPontuacao")];
-
-        document.getElementById("fourth-place").innerHTML = "";
-        document.getElementById("fourth-place-score").innerHTML = "";
-        document.getElementById("third-place").innerHTML = "";
-        document.getElementById("third-place-score").innerHTML = "";
-
-        document.getElementById("second-place").innerHTML = segundoColocado[0];
-        document.getElementById("second-place-score").innerHTML = segundoColocado[1] + " pontos";
-    }
 }
